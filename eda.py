b681741edcb573a350d2cb1dc977e2cd952eca32
Ayush Kumar <95694268+ayush078@users.noreply.github.com>
2025-07-28 17:37:18 +0530

    Update eda.py

--- eda.py
+++ eda.py
@@ -1,19 +1,3 @@
-# import pandas as pd
-
-# df = pd.read_csv("netflix_titles.csv")
-
-# print("Dataset Info:")
-# print(df.info())
-
-# print("\nMissing Values:")
-# print(df.isnull().sum())
-
-# print("\nDescriptive Statistics:")
-# print(df.describe(include='all'))
-
-# print("\nFirst 5 rows:")
-# print(df.head())
-
 import pandas as pd
 import numpy as np
 
